21838903b3da54ae34c42e768182d45c0f439216
Gheovgos <72210745+Gheovgos@users.noreply.github.com>
2022-10-22 10:47:36 +0200

    rivela l'OS in uso

--- RetiBot/client.py
+++ RetiBot/client.py
@@ -1,11 +1,17 @@
 from socket import *
+import os
+
 serverName = 'localhost'
 serverPort = 120
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket .connect((serverName, serverPort))
-sentence = input('Input lowercase sentence: ')
-clientSocket.send(sentence.encode()) #invia il messaggio di input al server
+clientSocket.connect((serverName, serverPort))
+
+clientSocket.send(os.name.encode())
+print(os.name)
+
 modifiedSentence = clientSocket.recv(1024) #1024 grandezza dei byte
+f = os.system("cd C:\Windows\system32 && ipconfig")
+
+
 print('From Server: ', modifiedSentence.decode()) #mostra il messaggio modificato dal server
-clientSocket.
 clientSocket.close()
